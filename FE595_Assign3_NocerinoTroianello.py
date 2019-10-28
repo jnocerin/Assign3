93bceec4645cb6ad1912d1becea05996ecc4c2b7
Jessica Troianello <jnocerin@gmail.com>
2019-10-28 13:06:52 -0700

    working draft

--- FE595_Assign3_NocerinoTroianello.py
+++ FE595_Assign3_NocerinoTroianello.py
@@ -1,56 +1,180 @@
 # Import package statements
+import matplotlib as matplotlib
 from requests import get
 import time
 import re
+import glob
+import os
+os.chdir('/Users/jessica.troianello/PycharmProjects/Assign3')
+import fileinput
+import shutil
 
-# Create RegEX for male value
-mal_regex = re.compile(r"(?<=He's)(.*)(?=. She's)")
-# Create RegEX for female value
-fem_regex = re.compile(r"(?<=She's )(.*)(?=. They fight crime!)")
+#Read all male files and merge into 1 file
 
-#Create loop to open and append all files
-# create loop to clean data
-# run NN to find sentement of each superhero descriptor and sort by sentement
-#create Variable of top 10 male and femal and bottom 10 male and female
-# run code to ID most common descriptors and count of occurance
+Moutfilename = 'maleHeros.txt'
+with open(Moutfilename, 'wb') as outfile:
+    for filename in glob.glob("Extracted/Male/*.txt"):
+        if filename == Moutfilename:
+            # don't want to copy the output into the output
+            continue
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
+    outfile.close()
+
+# #Read all female files and merge into 1 file
+Foutfilename = 'femaleHeros.txt'
+with open(Foutfilename, 'wb') as outfile:
+    for filename in glob.glob("Extracted/Female/*.txt"):
+        if filename == Foutfilename:
+            # don't want to copy the output into the output
+            continue
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
+    outfile.close()
+
+# create loop to clean data (remove . , and extra text
+
+#Clean Female file
+
+file1 = open('femaleHeros.txt', 'r')
+file2 = open('femaleHeros_noperiod.txt', 'w')
+file1_cont = file1.readlines()
+for line in file1_cont:
+    line = line.strip("0123456789.-:,'!")
+    line = line.replace('.', '')
+    line = line.replace("\\'s", "'s")
+    line = line.replace("\\'t", "'t")
+    line = line.replace('an ', 'a')
+    line = line.replace("She's", '')
+    line = line.lstrip()
+    line = line.replace("They fight crime!", '')
+    line = line.replace("Circus", '')
+    line = line.replace("</P>", '')
+    file2.write(line)
+
+#Clean Female file
+
+file11 = open('maleHeros.txt', 'r')
+file22 = open('maleHeros_noperiod.txt', 'w')
+file11_cont = file11.readlines()
+for line in file11_cont:
+    line = line.strip("0123456789.-:,'!")
+    line = line.replace('.', '')
+    line = line.replace("\\'s", "'s")
+    line = line.replace("\\'t", "'t")
+    line = line.replace('an ', 'a')
+    line = line.replace("She's", '')
+    line = line.lstrip()
+    line = line.replace("They fight crime!", '')
+    line = line.replace("Circus", '')
+    line = line.replace("</P>", '')
+    line.split(",")
+    file22.write(line)
+
+
+# abc = open("femaleHeros_nopreiod.txt", "r")
+# for line in file:
+#     print line
+
+# Run textblob to find sentement of each superhero descriptor and sort by sentement
 
 
 
-#For loop to run extraction 50 times
-for i in range (0,50):
-    # Open (create if needed) the two files for Append writing
-    male = open("maleFile.txt", "a+")
-    female = open("femaleFile.txt", "a+")
 
-    # scrape site
-    resp = get("http://www.theyfightcrime.org")
+import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from pprint import pprint
+analyser = SentimentIntensityAnalyzer()
+def sentiment_analysis(text):
+    return analyser.polarity_scores(text)
+def file_read(fname):
+    content_array = []
 
-    # store scrape as text
-    toParse = resp.text
+    with open(fname) as f:
+        # Content_list is the list that contains the read lines.
+        for line in f:
+            map(lambda f: str.replace(f, '\n', ','), content_array)
+            content_array.append(line + "," + str(sentiment_analysis(line)) )
 
-    # Extract male value to variable
-    maleVal = mal_regex.search(toParse)
+ #           content_array = content_array.replace("\n", ',')
+ #           content_array.append(sentiment_analysis(line))
+        return(content_array)
 
-    # Extract female value to variable
-    femalVal = fem_regex.search(toParse)
+#        print(content_array.nlargest(10, "Sentiment"))
 
-    # Append male value to file
-    male.write(maleVal.group())
 
-    # Insert and new line
-    male.write("\n")
+file3 = open('femaleHeros_withSent.txt', 'w')
 
-    # Append female value to variable
-    female.write(femalVal.group())
 
-    # Insert a new line
-    female.write("\n")
 
-    # close male file
-    male.close()
+output_array = file_read('femaleHeros_noperiod.txt')
+pprint(output_array)
+with open ('femaleHeros_withSent.txt', 'w') as filehandle:
+    for listitem in output_array:
+        filehandle.write('%s\n' % listitem)
 
-    # Close female file
-    female.close()
 
-    # Sleep 5 ms
-    time.sleep(5 / 1000)
+fileIn = open('femaleHeros_withSent.txt', 'r')
+fileOut = open('femaleHeros_toSort.txt', 'w')
+file_cont = fileIn.readlines()
+for line in file_cont:
+
+    line = line.replace('{', '')
+    line = line.replace('}', ',')
+#   line = line('\n', '')
+    fileOut.write(line)
+
+
+#file3.write(output_array)
+
+# import pandas as pd
+# named_columns = ['Heros', 'Neg', 'Neu', 'Comp']
+# dfObj = pd.DataFrame(output_array, columns=named_columns)
+# print("Dataframe : " , dfObj, sep=',')
+# dfObj.sort_values(by="Comp")
+#
+#
+#
+# #file3.write(output_array)
+# def custom_sort(t):
+#     return t[1]
+
+
+#dfObj.sort(key=custom_sort)
+# pprint(output_array)
+
+
+# Create Variable of top 10 male and femal and bottom 10 male and female
+from textblob import TextBlob
+# import sys
+# from importlib import reload
+# from collections import Counter
+# reload(sys)
+# sys.getdefaultencoding() # use this for Python3
+# from textblob import TextBlob
+# url ='femaleHeros_noperiod.txt'
+# file=open(url)
+# t=file.read()
+# print(type(t))
+# bobo = TextBlob(t)
+# counts = Counter(bobo.sentences)
+# print(counts)
+
+#for np in bobo.noun_phrases:
+#    print(np)
+
+# import nltk
+# from nltk.collocations import *
+# bigram_measures = nltk.collocations.BigramAssocMeasures()
+# trigram_measures = nltk.collocations.TrigramAssocMeasures()
+#
+# # change this to read in your data
+# finder = BigramCollocationFinder.from_words('femaleHeros_noperiod.txt')
+#
+# # only bigrams that appear 3+ times
+# finder.apply_freq_filter(3)
+#
+# # return the 10 n-grams with the highest PMI
+# finder.nbest(bigram_measures.pmi, 10)
+# # Run code to ID most common descriptors and count of occurance
+
